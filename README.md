887a14c6879d6b1a21c3c425177cae4d0d164d9e
root <Leihan Chen>
2023-11-13 17:00:11 -0500

    Initial neovim configuration for python

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 # neo_config
 The repo to store all my neo configuration info
+=======
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+>>>>>>> 46b79cd (Initial neovim configuration for python)
---
+++ lua/plugins/easy-commands.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "LintaoAmons/easy-commands.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<F5>",
+        "<cmd>DebugStartOrContinue<cr>",
+      },
+      {
+        "<F10>",
+        "<cmd>DebugStepInto<cr>",
+      },
+      {
+        "<F11>",
+        "<cmd>DebugStepOver<cr>",
+      },
+      {
+        "<C-<F5>>",
+        "<cmd>DebugStop<cr>",
+      },
+      {
+        "<C-<F10>>",
+        "<cmd>DebugRestart<cr>",
+      },
+      {
+        "<F6>",
+        "<cmd>DebugRunToCursor<cr>",
+      },
+    },
+    opts = {},
+  },
+  {
+    "LintaoAmons/scratch.nvim",
+    event = "VimEnter",
+  },
+}
