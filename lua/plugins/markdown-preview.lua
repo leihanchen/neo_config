975977d3f941dacfa6a33c77ad2dfa0e59a0bc50
Leihan Chen <leihan.chen@torontomu.ca>
2024-07-17 17:04:51 -0400

    Add markdown preview plugin

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,15 @@
+-- install with yarn or npm
+return {
+  {
+    "toppair/peek.nvim",
+    enabled = false,
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    ft = { "markdown" },
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+}
