5a811a700ecfd467b520ca27323ffe43474f8324
Leihan Chen <leihan.chen@torontomu.ca>
2024-09-01 00:38:32 -0400

    Disable autoformat and enable python language in lazy.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({"FileType"}, {
+    pattern = {"c", "cpp", "md", "txt", "c.snippets", "cpp.snippets"},
+    callback = function()
+        vim.b.autoformat = false
+        vim.opt_local.expandtab = true
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+    end,
+})
+
