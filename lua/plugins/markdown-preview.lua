286681a31a7f95f6085279747016aca887305d0c
Leihan Chen <leihan.chen@torontomu.ca>
2025-05-11 20:46:13 +0000

    Update markdown preview plugin

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,15 +1,50 @@
+-- return {
+--   "toppair/peek.nvim",
+--   event = { "VeryLazy" },
+--   build = "deno task --quiet build:fast",
+--   config = function()
+--     require("peek").setup({
+--       auto_load = true, -- whether to automatically load preview when
+--       -- entering another markdown buffer
+--       close_on_bdelete = true, -- close preview window on buffer delete
+--
+--       syntax = true, -- enable syntax highlighting, affects performance
+--
+--       theme = "dark", -- 'dark' or 'light'
+--
+--       update_on_change = true,
+--
+--       app = "webview", -- 'webview', 'browser', string or a table of strings
+--       -- explained below
+--
+--       filetype = { "markdown" }, -- list of filetypes to recognize as markdown
+--
+--       -- relevant if update_on_change is true
+--       throttle_at = 200000, -- start throttling when file exceeds this
+--       -- amount of bytes in size
+--       throttle_time = "auto", -- minimum amount of time in milliseconds
+--       -- that has to pass before starting new render
+--     })
+--     vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+--     vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+--   end,
+--   cmd = {"PeekOpen", "PeekClose" },
+-- }
 return {
-  {
-    "toppair/peek.nvim",
-    enabled = true,
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    ft = { "markdown", "md", ".md"},
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()     vim.fn["mkdp#util#install"]()
     end,
-  },
+    lazy = true,
+    keys = { { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+    config = function()
+      vim.g.mkdp_auto_close = true
+      vim.g.mkdp_open_to_the_world = false
+      vim.g.mkdp_port = "7860"
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_echo_preview_url = true
+      vim.g.mkdp_page_title = "${name}"
+      vim.g.mkdp_command_for_global = 1
+    end,
 }
-
