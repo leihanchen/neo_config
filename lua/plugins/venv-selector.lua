0382b24ebfb49edbdcd0eaf42b30589762f39fba
Leihan Chen <leihan.chen@torontomu.ca>
2025-05-12 21:23:52 +0000

    Update venv selector for new branch

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,14 +1,21 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  opts = function(_, opts)
-    if require("lazyvim.util").has("nvim-dap-python") then
-      opts.dap_enabled = true
-    end
-
-    return vim.tbl_deep_extend("force", opts, {
-      anaconda_base_path = "$HOME/miniconda3",
-      anaconda_envs_path = "$HOME/miniconda3/envs",
-      parents = 0,
-    })
-  end,
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" },
+  },
+  ---@type venv-selector.Config
+  opts = {
+    -- Your settings go here
+    anaconda_base_path = "$HOME/miniconda3",
+    anaconda_envs_path = "$HOME/miniconda3/envs",
+    parent = 0,
+  },
 }
